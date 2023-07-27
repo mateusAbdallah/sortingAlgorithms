8b5574e94b01fd89fd85436dea7eae056a9478c7
Mateus Abdallah <abdallahmateus@gmail.com>
2023-07-26 18:43:31 -0700

    add method to display the current status of the array

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class BubbleSort {
@@ -17,14 +15,14 @@ public class BubbleSort {
 			System.out.print("Enter number " + (a+1) + " >> ");
 			numbersArray[a] = input.nextInt();
 		}
-		System.out.println("***\s***\s***");
-		System.out.print("The initial array is: ");
-		for(int x: numbersArray) {
+		//System.out.println("***\s***\s***");
+		//System.out.print("The initial array is: ");
+		//for(int x: numbersArray) {
 
-			System.out.print(x + " ");
-		}
-		System.out.println();
-
+		//	System.out.print(x + " ");
+		//}
+		//System.out.println();
+		displayIteration(numbersArray, 0);
 		for(int i = 0; i < numbersArray.length - 1; i++) {
 			for(int j = 0; j < numbersArray.length - 1; j++) {
 				if(numbersArray[j] > numbersArray[j+1]) {
@@ -33,15 +31,23 @@ public class BubbleSort {
 					numbersArray[j+1] = temp;
 				}
 			}
+			displayIteration(numbersArray, (i+1));
 		}
-		System.out.println("***\s***\s***");
-		System.out.print("The sort array is: ");
-		for(int a : numbersArray) {
-			System.out.print(a + " ");
+		//System.out.println("***\s***\s***");
+		//System.out.print("The sort array is: ");
+		//for(int a : numbersArray) {
+		//	System.out.print(a + " ");
+		//} 
+
+	}
+	
+	static void displayIteration(int[] arr, int x) {
+		
+		System.out.print("Iteration " + x + ": ");
+		for(int c = 0; c < arr.length; c++) {
+			System.out.print(arr[c] + " ");
 		}
-
-
-
+		System.out.println();
 	}
 
 
