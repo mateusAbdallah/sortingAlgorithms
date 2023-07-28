2254a1b54f0cd93d39bf0316cc6917a9556274ef
Mateus Abdallah <abdallahmateus@gmail.com>
2023-07-27 23:03:28 -0700

    instanciate the insertionSort object

--- Main.java
+++ Main.java
@@ -2,9 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		BubbleSort bubble = new BubbleSort();
+		//BubbleSort bubble = new BubbleSort();
 		//System.out.println(bubble);
 
+		InsertionSort insertion = new InsertionSort();
 	}
 
 }
